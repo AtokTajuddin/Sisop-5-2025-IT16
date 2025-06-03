7369b0dbb48ccd2dd7c5b23cc0f3997aff0013e5
Mochammad Atha Tajuddin <mochathatajuddin03@gmail.com>
2025-06-03 22:49:42 +0700

    Update std_lib.h

--- include/std_lib.h
+++ include/std_lib.h
@@ -1,16 +1,16 @@
-#ifndef __STD_LIB_H__
-#define __STD_LIB_H__
+#ifndef STD_LIB_H
+#define STD_LIB_H
 
 #include "std_type.h"
 
 int div(int a, int b);
 int mod(int a, int b);
-
-bool strcmp(char *str1, char *str2);
+int mul(int a, int b); // Deklarasi baru untuk perkalian
+bool compareStrings(char *str1, char *str2);
+void strncpy(char *dst, char *src, int max_len);
 void strcpy(char *dst, char *src);
 void clear(byte *buf, unsigned int size);
-
 void atoi(char *str, int *num);
 void itoa(int num, char *str);
 
-#endif // __STD_LIB_H__
+#endif
